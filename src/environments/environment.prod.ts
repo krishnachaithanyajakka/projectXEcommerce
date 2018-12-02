ed4c46b8836bc1a3b5535afd0b29b54506344af5
Anirban <89.anirbandey@gmail.com>
2018-12-03 01:45:02 +0530

    aws key resolved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,7 @@ export const environment = {
   production: true,
   apiUrl: 'http://localhost:8080/'
 };
+export const awsKey = {
+  accessKeyId: 'AKIAID3M2SJEFWYM76QB',
+  secretAccessKey: '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',
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,10 @@ export const environment = {
   apiUrl: 'http://localhost:8080/',
 
 };
-
+export const awsKey = {
+  accessKeyId: 'AKIAID3M2SJEFWYM76QB',
+  secretAccessKey: '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',
+}
 // export const bucketPathsMap = {
 //   "banner 
 // }
