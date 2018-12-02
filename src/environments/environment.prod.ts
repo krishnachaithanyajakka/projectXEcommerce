842bed910a09a0fa818804a1690358995bcfffe0
Anirban <89.anirbandey@gmail.com>
2018-12-03 01:34:07 +0530

    heroku built conflict resolved

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://localhost:8080/'
 };
+export const awsKey = {
+  accessKeyId: 'AKIAID3M2SJEFWYM76QA',
+  secretAccessKey: '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',
+}
